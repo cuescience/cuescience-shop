432d7d05a783001d9ea5f93ca7440e964f042d2f
Ilja Bauer <iljabauer@gmail.com>
2015-07-21 17:50:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
         name='cuescience-shop',
-        version='0.2.9',
+        version='0.3',
         description='cuescience shop',
         maintainer='cuescience',
         maintainer_email='kontakt@cuescience.de',
